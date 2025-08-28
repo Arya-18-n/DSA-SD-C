17d884582b1471ab1816b3844ecfe601fa205a25
billlxclin <aryan9.verma09@gmail.com>
2025-08-29 00:40:12 +0530

    insert

--- Array/src/Access.java
+++ Array/src/Access.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class Access {
-
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-         int[] arr = new int[n];
 
-        System.out.println("Enter Array Elements");
+        System.out.print("Enter number of elements in array: ");
+        int n = sc.nextInt();
 
-         for (int i = 0; i < n; i++) {
-             arr[i] = sc.nextInt();
-         }
 
-        System.out.println("Array is :" );
-         for (int i = 0; i < n; i++) {
-             System.out.print(arr[i] + " ");
-         }
+        int[] arr = new int[n];
+
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        System.out.print("Array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
 }
